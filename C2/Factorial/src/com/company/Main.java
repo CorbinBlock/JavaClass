6ee3e6a5452f9dc684f29a8ffc7b5b8f3bfe916f
CorbinBlock <cblock0@gmail.com>
2019-05-14 15:50:04 -0500

    Initial Check-In

---
+++ C2/Factorial/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int factorial4 = 4 * 3 * 2 * 1;
+        System.out.println(factorial4);
+        int factorial7 = factorial4 * 5 * 6 * 7;
+        System.out.println(factorial7);
+
+    }
+}
