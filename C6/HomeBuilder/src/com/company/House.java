1520990721f7786e227edb0e35024c84be86ffd4
CorbinBlock <cblock0@gmail.com>
2019-05-29 09:47:59 -0500

    initial

---
+++ C6/HomeBuilder/src/com/company/House.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class House
+{
+    private int roomCount;
+
+    public int getRoomCount(int roomCount)
+    {
+        return roomCount;
+    }
+}
--- C6/People/src/com/company/Person.java
+++ C6/People/src/com/company/Person.java
@@ -2,5 +2,11 @@ package com.company;
 
 public class Person
 {
+    private String firstName;
+    private String lastName;
 
+    public String getfullName(String firstName, String lastName)
+    {
+        return lastName + ", " + firstName;
+    }
 }
